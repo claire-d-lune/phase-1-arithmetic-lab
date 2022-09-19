f77400edde5d55521022ac7ecbb34af6575fa421
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-19 15:21:55 -0600

    commit tree 52db4acdeb72db9e129a9bc64c81af2af942ec5f

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+// Write your code here`
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10 + 1);
+const num3 = 6
+const num4 = 34
+const mod = num4 % num3
+
+const max = Math.max(3,5,7,20, 14)
+// function random () {
+//     let randomNum = Math.floor((Math.random() * 10 + 1));
+//     let number = parseInt(randomNum, 10);
+//     return number;
+// }
+
+console.log(random());
